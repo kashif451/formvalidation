6ce4f3594a1c2160a023021a3c38fd3f1e37f379
Kashif <kashifmadni098@gmail.com>
2023-08-02 02:21:48 -0700

    Second commit

---
+++ form.js
@@ -0,0 +1,24 @@
+let userName = document.getElementById("username");
+// console.log(userName)
+let userPassword = document.getElementById("email");
+// console.log(userPassword)
+
+let userEmail = document.getElementById("password");
+// console.log(userEmail)
+
+
+let submit =document.getElementById("submit");
+// console.log(submit)
+
+
+userName.addEventListener("submit", function(){
+// function validation(){
+   // if
+  console.log("hello")
+   // {
+   // document.getElementById("error").innerHTML="username is empty"
+   // }
+// return false
+
+})
+
